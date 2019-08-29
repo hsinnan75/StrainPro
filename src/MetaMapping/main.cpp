0e0e3e4bea0b91e8fd80f2b55ef8609ccdcc846a
hsinnan75 <arith@iis.sinica.edu.tw>
2019-08-29 11:38:27 +0800

    deleted:    .gitignore 	modified:   src/MetaMapping/main.cpp

--- src/MetaMapping/main.cpp
+++ src/MetaMapping/main.cpp
@@ -18,9 +18,9 @@ int iThreadNum, iRefSeqNum, MaxMismatchNum, minFrequency, minDepth;
 void ShowProgramUsage(const char* program)
 {
 	fprintf(stderr, "\n");
-	fprintf(stderr, "MetaMapping v%s\n", VersionStr);
+	fprintf(stderr, "program v%s\n", program, VersionStr);
 	fprintf(stderr, "Usage: %s -i Index_Prefix -f <ReadFile_1 ReadFile_2 ...> -o OutputPrefix\n\n", program);
-	fprintf(stderr, "Options: IndexPrefix can be either an index prefix or a directory of multiple indices\n");
+	fprintf(stderr, "Options: IndexPrefix can be either an index prefix or a directory of multiple indexes\n");
 	fprintf(stderr, "         -t     INT     number of threads [%d]\n", iThreadNum);
 	fprintf(stderr, "         -n     INT     maximal mismatches in an alignment [%d]\n", MaxMismatchNum);
 	fprintf(stderr, "\n");
